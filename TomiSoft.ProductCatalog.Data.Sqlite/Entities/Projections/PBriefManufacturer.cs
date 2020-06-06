deb8822e40ff4946024840c0f29d82c606ddaa0b
std66 <sinkutamas@gmail.com>
2020-06-06 14:24:45 +0200

    Fix AutoMapper error for GET /Manufacturer API

--- TomiSoft.ProductCatalog.Data.Sqlite/Entities/Projections/PBriefManufacturer.cs
+++ TomiSoft.ProductCatalog.Data.Sqlite/Entities/Projections/PBriefManufacturer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TomiSoft.ProductCatalog.Data.Sqlite.Entities.Projections {
     internal class PBriefManufacturer {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public string LocationCountryCode { get; set; }
         public string LocationAddress { get; set; }
--- TomiSoft.ProductCatalog.Data.Sqlite/Mapping/PBriefManufacturerToBriefManufacturerBM.cs
+++ TomiSoft.ProductCatalog.Data.Sqlite/Mapping/PBriefManufacturerToBriefManufacturerBM.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using System;
 using TomiSoft.ProductCatalog.BusinessModels;
+using TomiSoft.ProductCatalog.BusinessModels.Concepts;
 using TomiSoft.ProductCatalog.Data.Sqlite.Entities.Projections;
 
 namespace TomiSoft.ProductCatalog.Data.Sqlite.Mapping {
@@ -8,8 +9,8 @@ namespace TomiSoft.ProductCatalog.Data.Sqlite.Mapping {
         public PBriefManufacturerToBriefManufacturerBM() {
             CreateMap<PBriefManufacturer, BriefManufacturerBM>()
                 .ForCtorParam("name", opt => opt.MapFrom(input => input.Name))
-                .ForCtorParam("manufacturerId", opt => opt.MapFrom(input => input.Id))
-                .ForCtorParam("websiteUri", opt => opt.MapFrom(input => new Uri(input.WebsiteUrl)))
+                .ForCtorParam("manufacturerId", opt => opt.MapFrom(input => new ManufacturerIdBM(input.Id)))
+                .ForCtorParam("websiteUri", opt => opt.MapFrom(input => input.WebsiteUrl == null ? null : new Uri(input.WebsiteUrl)))
                 .ForCtorParam("location", opt => opt.MapFrom(
                     input => new ManufacturerLocationBM(
                         input.LocationCountryCode,
